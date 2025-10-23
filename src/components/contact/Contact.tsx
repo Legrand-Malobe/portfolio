c1ac96f291315c87a9e7c027aa9df0d340dfa4cb
legrand.malobe <legrand.malobe@outlook.fr>
2025-10-23 04:07:51 +0100

    contact component

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,13 +1,42 @@
 import './contact.scss';
+import mailIcon from '../../assets/svg/contact/mail.svg';
+import linkedInIcon from '../../assets/svg/contact/linkedIn.svg';
+import githubIcon from '../../assets/svg/contact/github.svg';
+import { Link, Navigate } from 'react-router-dom';
 
 export default function Contact() {
+  const contactList = [
+    { id: 0, icon: mailIcon, value: 'legrand.malobe@outlook.fr' },
+    { id: 1, icon: linkedInIcon, value: 'Legrand Malobe', link: 'https://www.linkedin.com/in/legrand-malobe-3a0937244/' },
+    { id: 2, icon: githubIcon, value: 'Legrand-Malobe', link: 'https://github.com/Legrand-Malobe' },
+  ]
   return (
     <div className="contact-container" id="contact">
       <div className="contact-title">
         Contact
       </div>
       <div className="contact-content">
-          
+        {contactList.map((contact, index) => (
+          <div key={index} className="contact-item">
+            <div className="contact-icon">
+              <img src={contact.icon} alt="" />
+            </div>
+            <div className="contact-label">
+              {contact.id == 0 ?
+                <a href={`mailto:${contact.value}`}>
+                  <span>{contact.value}</span>
+                </a>
+                :
+                <Link to={contact.link!} target='blank'>
+                  <span>
+                    {contact.value}
+                  </span>
+                </Link>
+              }
+            </div>
+          </div>
+        ))
+        }
       </div>
     </div>
   )
